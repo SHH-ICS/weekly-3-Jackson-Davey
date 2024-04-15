61dc583e553911158dc6cb23573c52ffd2ce279c
Jackson-Davey <159150218+Jackson-Davey@users.noreply.github.com>
2024-04-15 17:18:09 +0000

    done

--- pizza.py
+++ pizza.py
@@ -3,17 +3,17 @@ import time
 toppingValue = [0,1,1.75,2.50,3.35]
 
 while True:
-    print("Welcome to papa's playfully patriotic and painstakingly passionate pizza parlor")
+    print("Welcome to Papa Davey's pizza parlor")
     time.sleep(2)
     print("What size pizza are you looking for today? (L or XL)")
     pizzaSize = str(input())
     if pizzaSize != "L" and pizzaSize != "XL":
-        print("Pizza must be either large or extra large! (please use capital letters)")
+        print("Pizza must be either large or extra large (please use capital letters)")
     elif pizzaSize == "L":
         print("How many toppings do you want (0 - 4)")
         topping = int(input())
         if topping > 4 or topping < 0:
-            print("Toppings must be within 0 and 4! (no decimals)")
+            print("Toppings must be within 0 and 4 (no decimals)")
         subTotal = 6 + toppingValue[topping]
         print("Subtotal is: $", subTotal)
         total = subTotal * 1.13
